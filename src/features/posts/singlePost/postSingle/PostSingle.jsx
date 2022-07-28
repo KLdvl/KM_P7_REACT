901b656c71c702bad37ddd7c41d3c8a53a0cecc6
Kevin Mas <maskevin27@gmail.com>
2022-07-28 20:28:13 +0200

    Delete post added

--- src/features/posts/singlePost/postSingle/PostSingle.jsx
+++ src/features/posts/singlePost/postSingle/PostSingle.jsx
@@ -1,14 +1,39 @@
 import './postSingle.scss'
+import {Link, useParams, useNavigate} from 'react-router-dom'
+import axios from 'axios'
+import {serverUrl} from "../../../variables/variables";
 
 export function PostSingle(props) {
+    let {id} = useParams()
+    const navigate = useNavigate()
+
     const displayPost = (props) => {
         const {post} = props;
-        if (post) {
+        const parsedStorage = JSON.parse(localStorage.user)
+        const deletePost = () => {
+            axios({
+                method: 'delete',
+                url: `${serverUrl}/post/${id}`,
+                mode: "cors",
+                headers: {
+                    'Authorization': `token ${parsedStorage.token}`
+                }
+            })
+                .then(res => navigate('/'))
+        }
+        if(post) {
             return (
                 <div className='post' key={post._id}>
                     <h3 className='post__title'>{post.title}</h3>
                     <p className='post__body'>{post.content}</p>
                     <img className="miniature" src={post.imageUrl}/>
+                    <div>
+                        <button>EDIT</button>
+                        <button onClick={deletePost}>DELETE</button>
+                        <Link to={"/"}>
+                            <button>GO BACK</button>
+                        </Link>
+                    </div>
                 </div>
             )
         } else {
